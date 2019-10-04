be9114700838dd5865d9acb4089adfcc31156abe
VitAndrGui <vitor.guidorizzi@hotmail.com>
2019-10-04 19:46:22 -0400

    pequeno fix

--- app/Http/Controllers/Endereco/AjaxController.php
+++ app/Http/Controllers/Endereco/AjaxController.php
@@ -1,7 +1,8 @@
 <?php /** @noinspection ALL */
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Endereco;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
