2ba12638ce1eb263aa55b78795732469678fcd7a
f3rnand0_lp@hotmail.com <f3rnand0_lp@hotmail.com>
2019-10-04 23:17:24 -0300

    Tela Inicial (Home)
    Adicionado elementos html seguindo o protótipo;
    Criado styles.css (css custom do projeto);

--- resources/views/layouts/app_with_footer.blade.php
+++ resources/views/layouts/app_with_footer.blade.php
@@ -20,6 +20,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     @stack('styles')
 
     <style>
